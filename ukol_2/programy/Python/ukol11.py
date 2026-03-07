09bb18a1b7951c97612043a7784d5195db510730
PapirovyBukanyr <101704235+PapirovyBukanyr@users.noreply.github.com>
2026-03-07 11:28:05 +0100

    code fix

--- ukol_2/programy/Python/ukol11.py
+++ ukol_2/programy/Python/ukol11.py
@@ -1,17 +1,22 @@
 import pulp
+
 #inicializace modelu
-model = pulp.LpProblem("Maximize_Zisku_Notip", pulp.LpMaximize)
+model = pulp.LpProblem("Maximize_Zisku", pulp.LpMaximize)
+
 #D-dřevěné a S-skleněné stoly
 pocet_D = pulp.LpVariable('x1', lowBound=0, cat='Integer')
 pocet_S = pulp.LpVariable('x2', lowBound=0, cat='Integer')
 
+#výplatní funkce
 model += 200*pocet_D + 350*pocet_S, "Zisk"
 
+#omezení
 model += 1*pocet_D <= 50, "Omezení_dřeva"
 model += 1*pocet_S <= 35, "Omezení_skla" 
 model += 5*pocet_D + 5*pocet_S <= 300, "Omezení_noh"
 model +=0.6*pocet_D + 1.5*pocet_S <= 63, "Omezení_času"
 
+#řešení modelu
 model.solve()
 print("--- VÝSLEDKY ---")
 print(f"zisk: {pulp.value(model.objective)}")
